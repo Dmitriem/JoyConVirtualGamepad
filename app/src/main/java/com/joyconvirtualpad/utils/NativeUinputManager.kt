056d186c06321a035cbfe01f5fe2d909a136d7c9
Dmitriem <incitatus18@gmail.com>
2025-08-24 13:38:13 +0300

    Update NativeUinputManager.kt

--- app/src/main/java/com/joyconvirtualpad/utils/NativeUinputManager.kt
+++ app/src/main/java/com/joyconvirtualpad/utils/NativeUinputManager.kt
@@ -1,1 +1,11 @@
+package com.joyconvirtualpad.utils
 
+object NativeUinputManager {
+    init {
+        System.loadLibrary("joyconvirtualpad")
+    }
+    
+    external fun createUinputDevice(): Int
+    external fun sendKeyEvent(fd: Int, code: Int, value: Int)
+    external fun destroyUinputDevice(fd: Int)
+}
