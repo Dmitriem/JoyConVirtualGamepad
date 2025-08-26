3c7ffcc04ed082ff7d9eaaa4ca22f2778dce470c
Dmitriem <incitatus18@gmail.com>
2025-08-26 19:16:54 +0300

    Update MainActivity.kt

--- app/src/main/java/com/joyconvirtualpad/MainActivity.kt
+++ app/src/main/java/com/joyconvirtualpad/MainActivity.kt
@@ -1,5 +1,52 @@
 package com.joyconvirtualpad
 
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Switch
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.joyconvirtualpad.utils.RootUtils
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var statusText: TextView
+    private lateinit var enableSwitch: Switch
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        
+        statusText = findViewById(R.id.tvStatus)
+        enableSwitch = findViewById(R.id.switchEnable)
+        
+        // Проверка root-прав
+        if (!RootUtils.isRootAvailable()) {
+            statusText.text = "Требуется root доступ!"
+            enableSwitch.isEnabled = false
+            return
+        }
+        
+        statusText.text = "Root доступ получен"
+        
+        enableSwitch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                // Запуск сервиса
+                val serviceIntent = Intent(this, JoyConMapperService::class.java)
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    startForegroundService(serviceIntent)
+                } else {
+                    startService(serviceIntent)
+                }
+                statusText.text = "Виртуальный геймпад включен"
+            } else {
+                // Остановка сервиса
+                val serviceIntent = Intent(this, JoyConMapperService::class.java)
+                stopService(serviceIntent)
+                statusText.text = "Виртуальный геймпад выключен"
+            }
+        }
+    }
+}package com.joyconvirtualpad
+
 import android.os.Bundle
 import android.widget.Switch
 import android.widget.TextView
