075102f6aa09cf850e8ceac57a53180b4d37db0f
Dmitriem <incitatus18@gmail.com>
2025-08-26 10:54:35 +0300

    Update MainActivity.kt

--- app/src/main/java/com/joyconvirtualpad/MainActivity.kt
+++ app/src/main/java/com/joyconvirtualpad/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.Switch
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.joyconvirtualpad.utils.RootUtils
 
 class MainActivity : AppCompatActivity() {
     private lateinit var statusText: TextView
@@ -16,12 +17,23 @@ class MainActivity : AppCompatActivity() {
         statusText = findViewById(R.id.tvStatus)
         enableSwitch = findViewById(R.id.switchEnable)
         
-        statusText.text = "Приложение запущено"
+        // Проверка root-прав
+        if (!RootUtils.isRootAvailable()) {
+            statusText.text = "Требуется root доступ!"
+            enableSwitch.isEnabled = false
+            return
+        }
+        
+        statusText.text = "Root доступ получен"
         
         enableSwitch.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
+                // Запуск сервиса с root-правами
+                RootUtils.executeCommand("am startservice -n com.joyconvirtualpad/.JoyConMapperService")
                 statusText.text = "Виртуальный геймпад включен"
             } else {
+                // Остановка сервиса
+                RootUtils.executeCommand("am stopservice com.joyconvirtualpad/.JoyConMapperService")
                 statusText.text = "Виртуальный геймпад выключен"
             }
         }
