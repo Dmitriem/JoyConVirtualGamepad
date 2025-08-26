10fd18d4e94782d71840803b9a87cb184f2f4eb0
Dmitriem <incitatus18@gmail.com>
2025-08-26 11:01:58 +0300

    Update RootUtils.kt

--- app/src/main/java/com/joyconvirtualpad/utils/RootUtils.kt
+++ app/src/main/java/com/joyconvirtualpad/utils/RootUtils.kt
@@ -1,13 +1,31 @@
 package com.joyconvirtualpad.utils
 
 import android.util.Log
+import java.io.DataOutputStream
+import java.io.IOException
 
 object RootUtils {
+    private const val TAG = "RootUtils"
+    
     fun isRootAvailable(): Boolean {
         return try {
-            // Простая проверка root доступа
             Runtime.getRuntime().exec("su").exitValue() == 0
         } catch (e: Exception) {
+            false
+        }
+    }
+    
+    fun executeCommand(command: String): Boolean {
+        return try {
+            val process = Runtime.getRuntime().exec("su")
+            val outputStream = DataOutputStream(process.outputStream)
+            outputStream.writeBytes("$command\n")
+            outputStream.writeBytes("exit\n")
+            outputStream.flush()
+            outputStream.close()
+            process.waitFor() == 0
+        } catch (e: IOException) {
+            Log.e(TAG, "Error executing root command", e)
             false
         }
     }
