4cf5cb6bcd0ea2dd68e551e729501d9f2b813900
Dmitriem <incitatus18@gmail.com>
2025-08-30 16:48:53 +0300

    Update native-lib.cpp

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,78 +1,101 @@
 #include <jni.h>
-#include <string>
-#include <android/log.h>
 #include <linux/uinput.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <cstring>
+#include <time.h>
+#include <android/log.h>
 
-#define LOG_TAG "JoyConNative"
+#define LOG_TAG "JoyConVirtualPad"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
 
+static void emit(int fd, uint16_t type, uint16_t code, int32_t value) {
+    struct input_event ie{};
+    ie.type = type;
+    ie.code = code;
+    ie.value = value;
+    clock_gettime(CLOCK_MONOTONIC, &ie.time);
+    write(fd, &ie, sizeof(ie));
+}
+
 extern "C" JNIEXPORT jint JNICALL
-Java_com_joyconvirtualpad_utils_NativeUinputManager_createUinputDevice(JNIEnv *env, jobject thiz) {
+Java_com_joyconvirtualpad_utils_NativeUinputManager_createUinputDevice(JNIEnv*, jobject) {
     int fd = open("/dev/uinput", O_WRONLY | O_NONBLOCK);
     if (fd < 0) {
-        LOGE("Error opening uinput device");
+        LOGE("Failed to open /dev/uinput");
         return -1;
     }
 
-    // Enable key events
+    // Кнопки
     ioctl(fd, UI_SET_EVBIT, EV_KEY);
-    for (int i = BTN_JOYSTICK; i <= BTN_THUMBR; i++) {
-        ioctl(fd, UI_SET_KEYBIT, i);
+    int buttons[] = {
+        BTN_SOUTH, BTN_EAST, BTN_NORTH, BTN_WEST, // A B X Y
+        BTN_TL, BTN_TR, BTN_TL2, BTN_TR2,         // L R L2 R2
+        BTN_THUMBL, BTN_THUMBR,                   // стики-кнопки
+        BTN_START, BTN_SELECT, BTN_MODE           // + - Home
+    };
+    for (int code : buttons) {
+        ioctl(fd, UI_SET_KEYBIT, code);
     }
 
-    // Enable absolute axes
+    // Оси
     ioctl(fd, UI_SET_EVBIT, EV_ABS);
-    ioctl(fd, UI_SET_ABSBIT, ABS_X);
-    ioctl(fd, UI_SET_ABSBIT, ABS_Y);
-    ioctl(fd, UI_SET_ABSBIT, ABS_Z);
-    ioctl(fd, UI_SET_ABSBIT, ABS_RZ);
-    ioctl(fd, UI_SET_ABSBIT, ABS_HAT0X);
-    ioctl(fd, UI_SET_ABSBIT, ABS_HAT0Y);
-
-    // Setup device
-    struct uinput_setup usetup = {};
-    usetup.id.bustype = BUS_USB;
-    usetup.id.vendor = 0x045e;  // Microsoft
-    usetup.id.product = 0x028e; // Xbox 360 Controller
-    strcpy(usetup.name, "Xbox 360 Wireless Receiver");
-
-    if (ioctl(fd, UI_DEV_SETUP, &usetup) < 0) {
-        LOGE("Error setting up uinput device");
-        close(fd);
-        return -1;
+    int axes[] = {ABS_X, ABS_Y, ABS_RX, ABS_RY, ABS_HAT0X, ABS_HAT0Y};
+    for (int code : axes) {
+        ioctl(fd, UI_SET_ABSBIT, code);
     }
 
-    if (ioctl(fd, UI_DEV_CREATE) < 0) {
-        LOGE("Error creating uinput device");
-        close(fd);
-        return -1;
-    }
+    // Настройки стиков
+    auto set_abs = [&](int code, int min, int max) {
+        struct uinput_abs_setup abs{};
+        abs.code = code;
+        abs.absinfo.minimum = min;
+        abs.absinfo.maximum = max;
+        abs.absinfo.fuzz = 16;
+        abs.absinfo.flat = 128;
+        ioctl(fd, UI_ABS_SETUP, &abs);
+    };
+    set_abs(ABS_X, -32768, 32767);
+    set_abs(ABS_Y, -32768, 32767);
+    set_abs(ABS_RX, -32768, 32767);
+    set_abs(ABS_RY, -32768, 32767);
+    set_abs(ABS_HAT0X, -1, 1);
+    set_abs(ABS_HAT0Y, -1, 1);
 
-    LOGI("Uinput device created successfully");
+    // Описание виртуального девайса
+    struct uinput_setup us{};
+    us.id.bustype = BUS_USB;
+    us.id.vendor  = 0x045e; // Microsoft
+    us.id.product = 0x028e; // Xbox 360 Controller
+    strcpy(us.name, "JoyCon Virtual Gamepad");
+
+    ioctl(fd, UI_DEV_SETUP, &us);
+    ioctl(fd, UI_DEV_CREATE);
+
+    LOGI("Virtual gamepad created (fd=%d)", fd);
     return fd;
 }
 
 extern "C" JNIEXPORT void JNICALL
-Java_com_joyconvirtualpad_utils_NativeUinputManager_sendKeyEvent(JNIEnv *env, jobject thiz, jint fd, jint code, jint value) {
-    struct input_event ev = {};
-    ev.type = EV_KEY;
-    ev.code = code;
-    ev.value = value;
-    write(fd, &ev, sizeof(ev));
-
-    struct input_event syn = {};
-    syn.type = EV_SYN;
-    syn.code = SYN_REPORT;
-    syn.value = 0;
-    write(fd, &syn, sizeof(syn));
+Java_com_joyconvirtualpad_utils_NativeUinputManager_sendKeyEvent(JNIEnv*, jobject, jint fd, jint code, jint value) {
+    if (fd > 0) emit(fd, EV_KEY, code, value);
 }
 
 extern "C" JNIEXPORT void JNICALL
-Java_com_joyconvirtualpad_utils_NativeUinputManager_destroyUinputDevice(JNIEnv *env, jobject thiz, jint fd) {
-    ioctl(fd, UI_DEV_DESTROY);
-    close(fd);
-    LOGI("Uinput device destroyed");
+Java_com_joyconvirtualpad_utils_NativeUinputManager_sendAbsEvent(JNIEnv*, jobject, jint fd, jint code, jint value) {
+    if (fd > 0) emit(fd, EV_ABS, code, value);
+}
+
+extern "C" JNIEXPORT void JNICALL
+Java_com_joyconvirtualpad_utils_NativeUinputManager_sync(JNIEnv*, jobject, jint fd) {
+    if (fd > 0) emit(fd, EV_SYN, SYN_REPORT, 0);
+}
+
+extern "C" JNIEXPORT void JNICALL
+Java_com_joyconvirtualpad_utils_NativeUinputManager_destroyUinputDevice(JNIEnv*, jobject, jint fd) {
+    if (fd > 0) {
+        ioctl(fd, UI_DEV_DESTROY);
+        close(fd);
+    }
 }
