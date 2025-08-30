054010209341155fc148bf4191d73009588c7bea
Dmitriem <incitatus18@gmail.com>
2025-08-30 16:41:47 +0300

    Update NativeUinputManager.kt

--- app/src/main/java/com/joyconvirtualpad/utils/NativeUinputManager.kt
+++ app/src/main/java/com/joyconvirtualpad/utils/NativeUinputManager.kt
@@ -1,15 +1,14 @@
 package com.joyconvirtualpad.utils
 
 object NativeUinputManager {
-    fun createUinputDevice(): Int {
-        return 1 // Заглушка для тестирования
+    init {
+        // Имя должно совпадать с add_library(...) в CMakeLists.txt
+        System.loadLibrary("joyconvirtualpad")
     }
-    
-    fun sendKeyEvent(fd: Int, code: Int, value: Int) {
-        // Заглушка для тестирования
-    }
-    
-    fun destroyUinputDevice(fd: Int) {
-        // Заглушка для тестирования
-    }
+
+    @JvmStatic external fun createUinputDevice(): Int
+    @JvmStatic external fun sendKeyEvent(fd: Int, code: Int, value: Int)
+    @JvmStatic external fun sendAbsEvent(fd: Int, code: Int, value: Int)
+    @JvmStatic external fun sync(fd: Int)
+    @JvmStatic external fun destroyUinputDevice(fd: Int)
 }
