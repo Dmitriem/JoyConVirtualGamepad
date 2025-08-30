fc8f7ff0e26ef23fdbf7a60a4caa166c04561df3
Dmitriem <incitatus18@gmail.com>
2025-08-30 16:12:42 +0300

    Update VirtualGamepadManager.kt

--- app/src/main/java/com/joyconvirtualpad/VirtualGamepadManager.kt
+++ app/src/main/java/com/joyconvirtualpad/VirtualGamepadManager.kt
@@ -8,6 +8,7 @@ class VirtualGamepadManager {
     
     fun createVirtualGamepad(): Boolean {
         return try {
+            Log.d("VirtualGamepadManager", "Creating virtual gamepad...")
             uinputFd = NativeUinputManager.createUinputDevice()
             if (uinputFd > 0) {
                 Log.d("VirtualGamepadManager", "Virtual gamepad created successfully")
@@ -17,27 +18,34 @@ class VirtualGamepadManager {
                 false
             }
         } catch (e: Exception) {
-            Log.e("VirtualGamepadManager", "Error creating virtual gamepad", e)
+            Log.e("VirtualGamepadManager", "Error creating virtual gamepad: ${e.message}", e)
             false
         }
     }
     
     fun sendKeyEvent(code: Int, value: Int) {
-        if (uinputFd <= 0) return
+        if (uinputFd <= 0) {
+            Log.e("VirtualGamepadManager", "Cannot send key event - virtual gamepad not created")
+            return
+        }
         
         try {
+            Log.d("VirtualGamepadManager", "Sending key event: code=$code, value=$value")
             NativeUinputManager.sendKeyEvent(uinputFd, code, value)
-            Log.d("VirtualGamepadManager", "Sent key event: code=$code, value=$value")
         } catch (e: Exception) {
-            Log.e("VirtualGamepadManager", "Error sending key event", e)
+            Log.e("VirtualGamepadManager", "Error sending key event: ${e.message}", e)
         }
     }
     
     fun destroy() {
         if (uinputFd > 0) {
-            NativeUinputManager.destroyUinputDevice(uinputFd)
-            uinputFd = -1
-            Log.d("VirtualGamepadManager", "Virtual gamepad destroyed")
+            try {
+                NativeUinputManager.destroyUinputDevice(uinputFd)
+                uinputFd = -1
+                Log.d("VirtualGamepadManager", "Virtual gamepad destroyed")
+            } catch (e: Exception) {
+                Log.e("VirtualGamepadManager", "Error destroying virtual gamepad: ${e.message}", e)
+            }
         }
     }
 }
